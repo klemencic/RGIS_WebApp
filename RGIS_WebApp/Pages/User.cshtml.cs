9e3f642b8c2a7cb14b9c399303185adfd0e00051
16bit2003 <87273633+16bit2003@users.noreply.github.com>
2023-12-29 17:27:17 +0100

    chat

---
+++ RGIS_WebApp/Pages/User.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RGIS_WebApp.Pages
+{
+    public class UserModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
