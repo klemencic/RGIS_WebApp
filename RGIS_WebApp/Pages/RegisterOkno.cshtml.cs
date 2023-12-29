2f469690d0e3a20be4a53b3c9d3016a81761380f
mark <mark.hajdih20@gmail.com>
2023-12-29 19:28:52 +0100

    s

--- RGIS_WebApp/Pages/RegisterOkno.cshtml.cs
+++ RGIS_WebApp/Pages/RegisterOkno.cshtml.cs
@@ -1,6 +1,7 @@
 using ClassDiagramCODScout;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
 
 namespace RGIS_WebApp.Pages
 {
@@ -29,13 +30,22 @@ namespace RGIS_WebApp.Pages
             if (ModelState.IsValid)
             {
                 Database database = new Database();
-
+                Random random = new Random();
                 Uporabnik uporabnik = new Uporabnik();
               uporabnik.Username = Username;
               uporabnik.Geslo = Password;
                 uporabnik.Email = Email;
                 uporabnik.IsModerator = false;
                 uporabnik.IsPremium = false;
+                uporabnik.Wins = random.Next(0, 30); 
+                uporabnik.Kills = random.Next(0, 100);
+                uporabnik.Deaths = random.Next(0, 100);
+                uporabnik.Matches = random.Next(0, 30);
+                uporabnik.Assists = random.Next(0, 100);
+                uporabnik.KDR = random.NextDouble() * 10.0;
+                uporabnik.Losses = random.Next(0, 30);
+
+
 
                 if (!database.ObjectExists(Username)){
 
